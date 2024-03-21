d5c732b283ecda8818741078aad057de3747d4ab
Petr Kavulok <petr.kavulok@ackee.cz>
2024-03-21 20:22:57 +0100

    ♻️ versions refactor

---
+++ apps/web/src/modules/versions/constants.ts
@@ -0,0 +1,19 @@
+export type VersionProps = {
+    title: 'CZ' | 'SK' | 'RO';
+    id: number;
+};
+
+export const versions = [
+    {
+        title: 'CZ',
+        id: 529093783,
+    },
+    {
+        title: 'SK',
+        id: 564759871,
+    },
+    {
+        title: 'RO',
+        id: 544688095,
+    },
+] as const satisfies ReadonlyArray<VersionProps>;
