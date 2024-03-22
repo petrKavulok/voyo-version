9e13faa25f4cbe8589870736f8d5495d8734ab3e
Petr Kavulok <petr.kavulok@ackee.cz>
2024-03-22 12:38:17 +0100

    ♻️ passing data from serversideprops refactor

--- apps/web/src/modules/versions/components/Versions/Versions.tsx
+++ apps/web/src/modules/versions/components/Versions/Versions.tsx
@@ -1,13 +1,17 @@
+import axios from 'axios';
+
 import { versions } from '~modules/versions/constants';
 
 import { Version } from '../Version';
 import * as Styled from './Versions.styles';
 
-export const VersionsContainer = () => {
+export const VersionsContainer = (props: any) => {
     return (
         <Styled.Container maxWidth='lg'>
             {versions.map(el => {
-                return <Version key={el.id} title={el.title} id={el.id} />;
+                const iosData = props.iosVersion?.[el.title.toLowerCase().toString()];
+
+                return <Version key={el.id} title={el.title} ios={iosData} />;
             })}
         </Styled.Container>
     );
