86cb0ee9711db183d2d10346bb821fadd24dc5a4
Petr Kavulok <petr.kavulok@ackee.cz>
2024-03-22 12:37:35 +0100

    ✨ serversideprops

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -2,13 +2,35 @@ import type { NextPage } from 'next';
 
 import { AppLayout } from '~modules/layout/components/AppLayout';
 import { VersionsContainer } from '~modules/versions/components';
+import { versions } from '~modules/versions/constants';
 
-const Home: NextPage = () => {
+const Home: NextPage = props => {
     return (
         <AppLayout>
-            <VersionsContainer />
+            <VersionsContainer {...props} />
         </AppLayout>
     );
 };
 
 export default Home;
+
+export async function getServerSideProps() {
+    const requests = [
+        fetch(`https://itunes.apple.com/lookup?id=${versions[0].id}`).then(response => response.json()),
+        fetch(`https://itunes.apple.com/lookup?id=${versions[1].id}`).then(response => response.json()),
+        fetch(`https://itunes.apple.com/lookup?id=${versions[2].id}`).then(response => response.json()),
+    ];
+
+    const [data1, data2, data3] = await Promise.all(requests);
+
+    // Pass fetched data to the page component props
+    return {
+        props: {
+            iosVersion: {
+                cz: data1,
+                sk: data2,
+                ro: data3,
+            },
+        },
+    };
+}
