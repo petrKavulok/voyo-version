c4e129b587b9a4aefc31841d8db71c8a2b259855
Petr Kavulok <petr.kavulok@ackee.cz>
2024-07-25 10:50:08 +0200

    ✨ scraping

--- apps/web/src/modules/versions/components/Versions/Versions.tsx
+++ apps/web/src/modules/versions/components/Versions/Versions.tsx
@@ -4,12 +4,14 @@ import { Version } from '../Version';
 import * as Styled from './Versions.styles';
 
 export const VersionsContainer = (props: any) => {
+    // console.log(props)
     return (
         <Styled.Container maxWidth='lg'>
             {versions.map(el => {
                 const iosData = props.iosVersion?.[el.title.toLowerCase().toString()];
+                const androidData = props.android?.[el.title.toLowerCase().toString()];
 
-                return <Version key={el.id} title={el.title} ios={iosData} />;
+                return <Version key={el.id} title={el.title} ios={iosData} android={androidData}/>;
             })}
         </Styled.Container>
     );
