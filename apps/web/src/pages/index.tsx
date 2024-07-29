0c8d9531cac7899f884b4b5e6a4900acb03ea1f4
Petr Kavulok <petr.kavulok@ackee.cz>
2024-07-29 10:26:05 +0200

    ♻️

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next';
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
+import chromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
 
 import { AppLayout } from '~modules/layout/components/AppLayout';
 import { VersionsContainer } from '~modules/versions/components';
@@ -17,7 +19,14 @@ export default Home;
 
 async function getLatestGoogleAppVersion(appId: string): Promise<string> {
     const url = `https://play.google.com/store/apps/details?id=net.cme.voyo.${appId}&hl=sk&pli=1`;
-    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch();
+
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
+
     const page = await browser.newPage();
   
     try {
@@ -54,32 +63,51 @@ async function getLatestGoogleAppVersion(appId: string): Promise<string> {
   }
 
 export async function getServerSideProps() {
-    const requests = [
-        fetch(`https://itunes.apple.com/lookup?id=${versions[0].id}`).then(response => response.json()),
-        fetch(`https://itunes.apple.com/lookup?id=${versions[1].id}`).then(response => response.json()),
-        fetch(`https://itunes.apple.com/lookup?id=${versions[2].id}`).then(response => response.json()),
-        getLatestGoogleAppVersion('cz'),
-        getLatestGoogleAppVersion('sk'),
-        getLatestGoogleAppVersion('ro'),
-    ];
+    try {
 
-    const [data1, data2, data3, 
-        data4, data5, data6
-    ] = await Promise.all(requests);
-
-    // Pass fetched data to the page component props
-    return {
-        props: {
-            iosVersion: {
-                cz: data1,
-                sk: data2,
-                ro: data3,
+        const requests = [
+            fetch(`https://itunes.apple.com/lookup?id=${versions[0].id}`).then(response => response.json()),
+            fetch(`https://itunes.apple.com/lookup?id=${versions[1].id}`).then(response => response.json()),
+            fetch(`https://itunes.apple.com/lookup?id=${versions[2].id}`).then(response => response.json()),
+            getLatestGoogleAppVersion('cz'),
+            getLatestGoogleAppVersion('sk'),
+            getLatestGoogleAppVersion('ro'),
+        ];
+        
+        const [data1, data2, data3, 
+            data4, data5, data6
+        ] = await Promise.all(requests);
+        
+        // Pass fetched data to the page component props
+        return {
+            props: {
+                iosVersion: {
+                    cz: data1,
+                    sk: data2,
+                    ro: data3,
+                },
+                android: {
+                    cz: data4,
+                    sk: data5,
+                    ro: data6,
+                }
             },
-            android: {
-                cz: data4,
-                sk: data5,
-                ro: data6,
-            }
-        },
-    };
+        };
+    } catch (e) {
+        console.error('Error in getServerSideProps:', e);
+        return {
+            props: {
+                iosVersion: {
+                    cz: null,
+                    sk: null,
+                    ro: null,
+                },
+                android: {
+                    cz: null,
+                    sk: null,
+                    ro: null,
+                },
+            },
+        };
+    }
 }
