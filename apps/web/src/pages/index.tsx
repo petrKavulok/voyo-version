c2bbc915bce3885e4b99d8c63a7685aebd5d571e
Petr Kavulok <petr.kavulok@ackee.cz>
2024-07-29 10:17:04 +0200

    ♻️

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -15,48 +15,45 @@ const Home: NextPage = props => {
 
 export default Home;
 
+async function getLatestGoogleAppVersion(appId: string): Promise<string> {
+    const url = `https://play.google.com/store/apps/details?id=net.cme.voyo.${appId}&hl=sk&pli=1`;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+  
+    try {
+      await page.goto(url, {
+        waitUntil: 'networkidle2',
+      });
 
+      // click button which opens modal with application's version    
+      await page.click('button[class="VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ QDwDD mN1ivc VxpoF"]');
+
+    //   Debugging: Take a screenshot to verify the page loaded correctly
+    //   await page.screenshot({ path: 'page.png' });
+
+      // Wait for the modal to load and display the version information
+      await page.waitForSelector('div.reAt0', { visible: true });
+
+      const version = await page.evaluate(() => {
+        const versionElement = document.querySelector('div.reAt0');
+        return versionElement ? versionElement.textContent : 'Version not found';
+      });
+  
+      await browser.close();
+  
+      if (!version) {
+        throw new Error('Version element not found');
+      }
+  
+      return version;
+    } catch (error) {
+      await browser.close();
+      console.error('Error fetching the latest version:', error);
+      throw error;
+    }
+  }
 
 export async function getServerSideProps() {
-    async function getLatestGoogleAppVersion(appId: string): Promise<string> {
-        const url = `https://play.google.com/store/apps/details?id=net.cme.voyo.${appId}&hl=sk&pli=1`;
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-      
-        try {
-          await page.goto(url, {
-            waitUntil: 'networkidle2',
-          });
-    
-          // click button which opens modal with application's version    
-          await page.click('button[class="VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ QDwDD mN1ivc VxpoF"]');
-    
-        //   Debugging: Take a screenshot to verify the page loaded correctly
-        //   await page.screenshot({ path: 'page.png' });
-    
-          // Wait for the modal to load and display the version information
-          await page.waitForSelector('div.reAt0', { visible: true });
-    
-          const version = await page.evaluate(() => {
-            const versionElement = document.querySelector('div.reAt0');
-            return versionElement ? versionElement.textContent : 'Version not found';
-          });
-      
-          await browser.close();
-      
-          if (!version) {
-            throw new Error('Version element not found');
-          }
-      
-          return version;
-        } catch (error) {
-          await browser.close();
-          console.error('Error fetching the latest version:', error);
-          throw error;
-        }
-      
-      }
-
     const requests = [
         fetch(`https://itunes.apple.com/lookup?id=${versions[0].id}`).then(response => response.json()),
         fetch(`https://itunes.apple.com/lookup?id=${versions[1].id}`).then(response => response.json()),
